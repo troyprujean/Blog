d3e8d3ecbf6cf2b1265ecea3e68a4586937a00f0
troy <troy.prujean@radfords.co.nz>
2022-08-26 22:09:14 +1200

    create context and provider

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
+import { Provider } from './src/context/BlogContext';
 
 const navigator = createStackNavigator({
   Index: IndexScreen
@@ -11,4 +13,10 @@ const navigator = createStackNavigator({
   }
 });
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <Provider>
+      <App />
+  </Provider>
+};
